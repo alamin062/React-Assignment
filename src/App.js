9d59db7893152be8a9e75a13fabe717a01c318b9
Al Amin PC <alaminrahman0106@gmaile.com>
2022-07-29 12:24:44 +0600

    React Assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/home" />}></Route>
           <Route path="/home" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
